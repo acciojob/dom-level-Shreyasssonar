08d04bc129c30cbda757f862105b30868b253355
Shreyas <121279572+Shreyasssonar@users.noreply.github.com>
2023-08-23 08:38:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+// Find the element with the ID 'level'
+const targetElement = document.getElementById('level');
+
+// Calculate the DOM level by counting preceding siblings
+function calculateDOMLevel(element) {
+  let level = 0;
+  while (element.previousElementSibling) {
+    level++;
+    element = element.previousElementSibling;
+  }
+  return level;
+}
+
+// Calculate the DOM level of the target element
+const domLevel = calculateDOMLevel(targetElement);
+
+// Display the DOM level using an alert
+alert(`The level of the element is: ${domLevel}`);
